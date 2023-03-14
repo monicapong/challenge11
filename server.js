84494d867e6d2faf108ee6345900ea4226b2da7b
monicapong <111400935+monicapong@users.noreply.github.com>
2023-03-14 10:56:20 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+// Import node modules 
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+// Initialize app variable
+const app = express();
+
+// Sets up the Express app middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route to direct users to the homepage
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhots:${PORT}`)
+});
